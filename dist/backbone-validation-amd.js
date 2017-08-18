42b2657e3cc6e68c8a462fb79b9d03c2ff85f86c
Kiran Patil <patil.kiran.m@gmail.com>
2017-08-18 11:16:53 +0300

    Adding dist files
    Signed-off-by: Kiran Patil <patil.kiran.m@gmail.com>

--- dist/backbone-validation-amd.js
+++ dist/backbone-validation-amd.js
@@ -1,6 +1,6 @@
 // Backbone.Validation v0.11.5
 //
-// Copyright (c) 2011-2015 Thomas Pedersen
+// Copyright (c) 2011-2017 Thomas Pedersen
 // Distributed under MIT License
 //
 // Documentation and full license available at:
@@ -257,9 +257,10 @@
             if (attrs) {
               flattened = flatten(this.attributes);
               //Loop through all associated views
-              _.each(this.associatedViews, function(view) {
+              var that = this;
+              _.each(that.associatedViews, function(view) {
                 _.each(attrs, function (attr) {
-                  error = validateAttr(this, attr, flattened[attr], _.extend({}, this.attributes));
+                  error = validateAttr(that, attr, flattened[attr], _.extend({}, that.attributes));
                   if (error) {
                     options.invalid(view, attr, error, options.selector);
                     invalidAttrs = invalidAttrs || {};
@@ -267,8 +268,8 @@
                   } else {
                     options.valid(view, attr, options.selector);
                   }
-                }, this);
-              }, this);
+                });
+              });
             }
   
             if(option === true) {
